b006314c62c480ece8d4369482607606d3e92b06
Richard Fergie <richard.fergie+github@gmail.com>
2016-07-13 15:09:03 +0100

    Attempt to figure out eastings
    Big comment on procedure, which seems to make sense and match up with wiki
    
    But can't get it to work in practice yet

--- kg_coords.py
+++ kg_coords.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import pyproj
-
+import math
 
 def degminsec_to_decimal(dms):
     (degrees, minutes, seconds) = dms
@@ -32,6 +32,25 @@ objectives = [
         ("Objective D", (42, 6, 58.28), (78, 52, 37.21), wgs84),
         ("Centre point of map", (42, 5, 0), (78, 52, 30), sk42),
     ]
+
+# Definition of the easting from https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%B2%D0%B5%D1%82%D1%81%D0%BA%D0%B0%D1%8F_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0_%D1%80%D0%B0%D0%B7%D0%B3%D1%80%D0%B0%D1%84%D0%BA%D0%B8_%D0%B8_%D0%BD%D0%BE%D0%BC%D0%B5%D0%BD%D0%BA%D0%BB%D0%B0%D1%82%D1%83%D1%80%D1%8B_%D1%82%D0%BE%D0%BF%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D1%85_%D0%BA%D0%B0%D1%80%D1%82#.D0.9F.D1.80.D1.8F.D0.BC.D0.BE.D1.83.D0.B3.D0.BE.D0.BB.D1.8C.D0.BD.D0.B0.D1.8F_.28.D0.BA.D0.B8.D0.BB.D0.BE.D0.BC.D0.B5.D1.82.D1.80.D0.BE.D0.B2.D0.B0.D1.8F.29_.D1.81.D0.B5.D1.82.D0.BA.D0.B0_.D0.BA.D0.BE.D0.BE.D1.80.D0.B4.D0.B8.D0.BD.D0.B0.D1.82
+# 1. The first one or two digits is the number of the zone.
+#    In our case this will be 44 because our map is K-44-062-Г
+# 2. The rest is the number of meters relative to the central meridian of the zone. 500,000 is the central meridian
+#    with west of that being higher numbers and east of that being lower numbers
+#    K44 runs from 78 degrees to 84 degrees (http://maps.vlasenko.net/smtm1000/k-44.jpg)
+#    So the central meridian is 81 degrees.
+#    This seems to match with checking maps with grid numbers and not just lat/lng.
+#    e.g. http://maps.vlasenko.net/smtm500/k-44-1.jpg
+#    This shows 81 degrees exactly lying on easting 14500. Which doesn't really match exactly with
+#    what I'd expect but it does finish with 00.
+#
+#    Have no idea if "distance" here means great circle distance or distance travelling parallel
+#    to the equator although I'd guess the latter
+def distance_along_latitude(lat,lng1,lng2):
+    DEGREE_LENGTH_AT_EQUATOR=111321 #111.321 km
+    return (lng1-lng2)*DEGREE_LENGTH_AT_EQUATOR*math.cos(lat*math.pi/180)
+
 
 for (name, lat_triple, lon_triple, projection) in objectives:
     lat = degminsec_to_decimal(lat_triple)
